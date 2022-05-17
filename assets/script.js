a1b106a607060b7b897c0412531f50c66559acb7
Droessling94 <101940633+Droessling94@users.noreply.github.com>
2022-05-16 23:22:56 -0400

    updated portfolio to be more compact, got rid of unneccasry buttons and event handlers associated with them

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,12 @@
 
-//Navbar Button Event Listeners for content switch//
-const aboutBtn = document.querySelector("#aboutBtn")
-const contactBtn = document.querySelector("#contactBtn")
-const resumeBtn = document.querySelector("#resumeBtn")
-const workBtn = document.querySelector("#workBtn")
 const projOne = document.querySelector("#projOne")
 const projTwo = document.querySelector("#projTwo")
-const leftDisplay =document.querySelector("#leftDisplayContainer")
 
-aboutBtn.addEventListener('click', function(){
-    $("#leftDisplayContent").text("Hello, My name is Donaldson but I prefer to go by Donnie. For the past ten years I've worked in the service & sales industry. During that time I developed valuable communication and organization skills. I have since left the service and sales industry to pursue a career in the tech industry. I am attending UCF's full time full-stack coding bootcamp to help this transition, and I am excited to see what team I can benefit.");
-});
-
-contactBtn.addEventListener('click', function(){
-    $("#leftDisplayContent").text("links go here");
-});
-
-workBtn.addEventListener('click', function(){
-    $("#leftDisplayContent").text("Also coming soon");
-});
 // Event listeners to redirect to project repositories//
 projOne.addEventListener('click', function(){
     window.location.href="https://droessling94.github.io/IDunno/";
 });
 
 projTwo.addEventListener('click', function(){
-    window.location.href="https://droessling94.github.io/IDunno/";
+    window.location.href="https://agile-wildwood-01808.herokuapp.com/";
 });
